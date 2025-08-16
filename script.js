f31866a0cdbb28075b142b43b0ff5b42fcb2305f
zlucasftw <ilucasmcpvp@gmail.com>
2025-08-16 05:41:34 -0300

    feat: header responsiveness, projects base page, projects responsive image and overall styling fixing

--- script.js
+++ script.js
@@ -8,12 +8,14 @@ const buttons = document.querySelectorAll("[data-carousel-button]");
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-        const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
+        const slides = button
+            .closest("[data-carousel]")
+            .querySelector("[data-slides]");
     
 
-        const activeSlide = slides.querySelectorAll("[data-active]");
+        const activeSlide = slides.querySelector("[data-active]");
     
-        let newIndex = [...slides.children].indexOf(activeSlide[0]) + offset;
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
         if (newIndex < 0) {
             newIndex =  slides.children.length - 1;
         }
@@ -25,3 +27,24 @@ buttons.forEach(button => {
         delete activeSlide.dataset.active;
     });
 });
+
+/* const navOpenButton = document.querySelector("#open__nav");
+const navCloseButton = document.querySelector("#close__nav");
+
+navOpenButton.addEventListener("click", () => {
+    navOpenButton.setAttribute("aria-expanded", "true");
+});
+
+navCloseButton.addEventListener("click", () => {
+    navCloseButton.setAttribute("aria-expanded", "false");
+}); */
+
+const navbar = document.getElementById("navbar");
+
+function openSideBar() {
+    navbar.classList.add("show");
+}
+
+function closeSideBar() {
+    navbar.classList.remove("show");
+}
